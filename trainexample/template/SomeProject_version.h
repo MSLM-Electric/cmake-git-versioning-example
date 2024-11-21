45d095b913e9750b450ec5a20dee9c0db078051d
Osim A <g-unit_osim@mail.ru>
2024-11-21 20:42:08 +0500

    generator tested sucessfuly!

--- trainexample/template/SomeProject_version.h
+++ trainexample/template/SomeProject_version.h
@@ -1,4 +1,4 @@
 #ifndef SomeProject_VERSION_H_
 #define SomeProject_VERSION_H_
-#define SomeProject_VERSION "v0.0.3b-train-2-g5ad9e0d-dirty"
-#endif  /* FOOBAR_VERSION_H_ */
+#define SomeProject_VERSION "v0.0.3b-train-3-g6908533-dirty"
+#endif  /*  */
